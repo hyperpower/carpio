c4f9ee6ca5046d2fbb432d184cb62c3b05eafd36
HyperPower <hyper__power@hotmail.com>
2015-10-04 21:51:07 +0200

    first

---
+++ carpio_define.hpp
@@ -0,0 +1,41 @@
+#ifndef _DEFINE_HPP_
+#define _DEFINE_HPP_
+
+#include <array>
+#include <deque>
+#include <forward_list>
+#include <list>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <unordered_map>
+
+
+namespace carpio{
+
+#define ASSERT(expr) assert(expr)
+#define ASSERT_MSG(expr, msg) assert((expr)&&(msg))
+#define CAST(type, p)          ((type)p)
+#define CAST_REF(type, p)      (*((type)p))
+#define _IF_TRUE_RETRUN(expr)  if(expr){return;};
+#define _IF_FALSE_RETRUN(expr)  if(false==(expr)){return;};
+// value type
+typedef std::size_t st;
+typedef double Float;
+typedef void* utPointer;
+typedef const void* const_utPointer;
+
+
+// std  container
+template<typename T>
+using Vector = std::vector<T>;
+
+
+
+
+
+}
+
+
+#endif
